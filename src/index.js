09e8111ee7607ecd024b09d6569105f7912f3c37
Shubham Tiwari <websbiz0000@gmail.com>
2019-04-03 16:26:33 +0000

    uploader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Fileupload from './Fileupload';
 import {render} from 'react-dom';
 
 function App(){
     return(
         <div>
-            Welcome to React.
+            <Fileupload/>
         </div>
     )
 }
